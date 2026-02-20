fdf658d0c5a3327d81b947cc96ac780b51dad670
icaroMendes777 <114268516+icaroMendes777@users.noreply.github.com>
2026-02-20 09:36:36 -0300

    feat: get product by id

--- src/application/product/get-products.usecase.ts
+++ src/application/product/get-products.usecase.ts
@@ -3,9 +3,13 @@ import { ProductRepository } from '../../domain/product/product.repository';
 
 @Injectable()
 export class GetProductsUseCase {
-  constructor(private readonly repository: ProductRepository) {}
+    constructor(private readonly repository: ProductRepository) { }
 
-  async execute() {
-    return this.repository.findAll();
-  }
+    async execute() {
+        return this.repository.findAll();
+    }
+
+    async find(id: number) {
+        return this.repository.findById(id);
+    }
 }
--- src/presentation/product/product.controller.ts
+++ src/presentation/product/product.controller.ts
@@ -1,21 +1,26 @@
-import { Controller, Get, Post, Body } from '@nestjs/common';
+import { Controller, Get, Post, Body, Param, ParseIntPipe } from '@nestjs/common';
 import { CreateProductUseCase } from '../../application/product/create-product.usecase';
 import { GetProductsUseCase } from '../../application/product/get-products.usecase';
 
 @Controller('products')
 export class ProductController {
-  constructor(
-    private readonly createProduct: CreateProductUseCase,
-    private readonly getProducts: GetProductsUseCase,
-  ) {}
+    constructor(
+        private readonly createProduct: CreateProductUseCase,
+        private readonly getProducts: GetProductsUseCase,
+    ) { }
 
-  @Post()
-  create(@Body() body: any) {
-    return this.createProduct.execute(body);
-  }
+    @Post()
+    create(@Body() body: any) {
+        return this.createProduct.execute(body);
+    }
 
-  @Get()
-  findAll() {
-    return this.getProducts.execute();
-  }
+    @Get()
+    findAll() {
+        return this.getProducts.execute();
+    }
+
+    @Get(':id')
+    findProduct(@Param('id', ParseIntPipe) id: number,) {
+        return this.getProducts.find(id);
+    }
 }
